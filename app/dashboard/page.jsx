732bc89c0c9e9a6edb9f136b7768525d172b970b
naybyal <officiallynabiel@gmail.com>
2024-03-24 20:41:36 +0530

    Configuring Main section and Rightbar

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1,6 +1,23 @@
+import styles from '../ui/dashboard/dashboard.module.css'
+import Card from "@/app/ui/dashboard/card/card";
+import Transactions from "@/app/ui/dashboard/transactions/transactions";
+import Rightbar from "@/app/ui/dashboard/rightbar/rightbar";
 function Dashboard() {
     return (
-        <></>
+        <div className={styles.wrapper}>
+            <div className={styles.main}>
+                <div className={styles.cards}>
+                    <Card />
+                    <Card />
+                    <Card />
+                </div>
+                <Transactions />
+                <Chart />
+            </div>
+            <div className={styles.side}>
+                <Rightbar />
+            </div>
+        </div>
     );
 }
 
