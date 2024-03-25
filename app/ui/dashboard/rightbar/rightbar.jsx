bb0b565ee4b1068a83d62c8ffee3b18b3f9ca511
naybyal <officiallynabiel@gmail.com>
2024-03-25 21:21:11 +0530

    Configuring Charts

--- app/ui/dashboard/rightbar/rightbar.jsx
+++ app/ui/dashboard/rightbar/rightbar.jsx
@@ -2,7 +2,11 @@ import styles from './rightbar.module.css'
 
 const Rightbar = () => {
     return (
-        <div className={styles.container}></div>
+        <div className={styles.container}>
+            <div className={styles.item}>
+
+            </div>
+        </div>
     );
 }
 
