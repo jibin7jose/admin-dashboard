4afc6c1b995b0889dfc740f70c4f1c6902b0b0e4
naybyal <officiallynabiel@gmail.com>
2024-03-28 09:22:50 +0530

    Configuring Abuse Recorder Front-end.

--- app/ui/dashboard/rightbar/rightbar.jsx
+++ app/ui/dashboard/rightbar/rightbar.jsx
@@ -1,39 +1,27 @@
 import styles from './rightbar.module.css'
-import Astronaut from 'public/astronaut.png'
+import Abuse from 'public/abuse.png'
 import Image from 'next/image'
-import {MdEmergencyRecording} from "react-icons/md";
+import {MdCrisisAlert, MdEmergencyRecording} from "react-icons/md";
 const Rightbar = () => {
     return (
         <div className={styles.container}>
             <div className={styles.item}>
                 <div className={styles.bgContainer}>
-                    <Image className={styles.bg} src={Astronaut} alt="" fill/>
+                    <Image className={styles.bg} src={Abuse} alt="" fill/>
                 </div>
-                <div className={styles.texts}>
-                    <span className={styles.notification}>Report Abuse!</span>
+                <div className={styles.text}>
+                    <span className={styles.notification}>🚨 Report Abuse!</span>
                     <h3>Facing harassment from a customer?</h3>
-                    <span>Record the instance.</span>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam aliquid atque consequuntur
-                        debitis ducimus, eos esse expedita fuga, id magnam nemo neque quidem saepe, sapiente vel.
-                        Aspernatur ea incidunt iusto?</p>
-                    <button className={styles.button}>
-                        <MdEmergencyRecording/>Record
-                    </button>
+                    <span>Record the instance</span>
+                    <div className={styles.buttons}>
+                        <button className={styles.button}>
+                            <MdEmergencyRecording/>Start Recording
+                        </button>
+                    </div>
+
                 </div>
             </div>
-            <div className={styles.item}>
-                <div className={styles.texts}>
-                    <span className={styles.notification}>Report Abuse!</span>
-                    <h3>Facing harassment from a customer?</h3>
-                    <span>Record the instance.</span>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam aliquid atque consequuntur
-                        debitis ducimus, eos esse expedita fuga, id magnam nemo neque quidem saepe, sapiente vel.
-                        Aspernatur ea incidunt iusto?</p>
-                    <button className={styles.button}>
-                        <MdEmergencyRecording/>Record
-                    </button>
-                </div>
-            </div>
+
         </div>
     );
 }
