6b5771d59579bcfe5335e0deca8897922081b3aa
naybyal <officiallynabiel@gmail.com>
2024-04-04 12:03:51 +0530

    Configuring the Add Services Page

--- app/dashboard/services/add/page.jsx
+++ app/dashboard/services/add/page.jsx
@@ -1,30 +1,61 @@
+"use client"
+
 import styles from '../../../ui/dashboard/staffs/addStaff/add.module.css'
+import {useState} from "react";
+import {useRouter} from "next/navigation";
+
 const AddService = () => {
+    const [serviceName, setServiceName] = useState('');
+    const [serviceCost, setServiceCost] = useState(null);
+    const [serviceProfit, setServiceProfit] = useState(null);
+    const [serviceLink, setServiceLink] = useState('');
+
+    const router = useRouter();
+
+    const handleServiceNameChange = (event) => {
+        setServiceName(event.target.value);
+    }
+
+    const handleServiceCostChange = (event) => {
+        setServiceCost(event.target.value);
+    }
+
+    const handleServiceProfitChange = (event) => {
+        setServiceProfit(event.target.value);
+    }
+
+    const handleServiceLinkChange = (event) => {
+        setServiceLink(event.target.value);
+    }
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+
+        try {
+            await fetch('../../../api/addService', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({serviceName, serviceCost, serviceProfit, serviceLink}) })
+
+            router.refresh()
+        } catch (error) {
+            console.error(error);
+        }
+
+        setServiceName('');
+        setServiceCost(null);
+        setServiceProfit(null);
+        setServiceLink('');
+    }
     return (
         <div className={styles.container}>
-            <form action="" className={styles.form}>
-
-                <input type="text" placeholder='First Name' name='fname' required/>
-
-                <input type="text" placeholder='Last Name' name='lname' required/>
-
-                <select name="role" id="role">
-                    <option value="general">
-                        Choose Role
-                    </option>
-                    <option value="m">
-                        M-Staff
-                    </option>
-                    <option value="c">
-                        C-Staff
-                    </option>
-                </select>
-
-                <input type='number' name="aadhar" id="aadhar" placeholder='Aadhar No.'/>
-                <input type='number' name="phone" id="phone" placeholder='Phone No.'/>
-
-                <input type="number" id='salary' placeholder='Salary (INR)' name='salary'/>
-                <textarea name="address" id="address" rows='16' placeholder='Address'></textarea>
+            <form onSubmit={handleSubmit} className={styles.form}>
+                <input type="text" placeholder='Service Name' name='serviceName' value={serviceName} onChange={handleServiceNameChange} required />
+                <input type='number' name="serviceCost" id="serviceCost" placeholder='Service Cost' value={serviceCost.toString()} required/>
+                <input type='number' name="serviceProift" id="serviceProfit" placeholder='Service Profit' value={serviceProfit.toString()} required/>
+                <textarea name="serviceLink" id="serviceLink" rows='1' placeholder='Service Link' value={serviceLink}></textarea>
                 <button type="submit">Submit</button>
             </form>
         </div>
